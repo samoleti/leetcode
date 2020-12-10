b3027c49a0ea62f759402b6d35b9722f1c922b33
Violeta Pavlova <lety1006@gmail.com>
2020-12-10 15:24:12 +0200

    344. Reverse String

---
+++ reverse-string/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int size = s.size();
+        for(int i = 0; i < s.size()/2; i++)
+        { 
+            swap(s[i],s[size - i - 1]);
+        }
+        
+    }
+};
